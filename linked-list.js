a342c622b864e87b9942539410f087d713f052a6
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-06-12 12:20:24 -0700

    on insert

--- linked-list.js
+++ linked-list.js
@@ -27,29 +27,44 @@ class LinkedList {
 
     if (this.head === null) this.head = node;
     if (this.tail !== null) this.tail.next = node;
-    
-    this.tail = node
+
+    this.tail = node;
+    this.length++;
+    return undefined;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
     let node = new Node(val);
-    
+
     if (this.head !== null) node.next = this.head;
     if (this.tail === null) this.tail = node;
 
     this.head = node;
+    this.length++;
+    return undefined;
   }
 
   /** pop(): return & remove last item. */
 
   pop() {
+    if (this.length === 0) throw new Error("empty list");
+    if (this.length === 1) {
+      let returnValue = this.tail;
+
+      this.head = null;
+      this.tail = null;
+
+      this.length--;
+      return returnValue.val;
+    }
+
 
     let currNode = this.head;
 
-    while(node.next !== this.tail) {
-      currNode = this.next;
+    while (currNode.next !== this.tail) {
+      currNode = currNode.next;
 
     }
 
@@ -57,35 +72,99 @@ class LinkedList {
     this.tail = currNode;
     this.tail.next = null;
 
-    return returnValue;
+    this.length--;
+    return returnValue.val;
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
+    if (!this.length) throw new Error("empty list");
+    // if (this.length === 1) {
+    //   let returnValue = this.tail;
+
+    //   this.head = null;
+    //   this.tail = null;
+
+    //   this.length--;
+    //   return returnValue.val;
+    // }
+
+
     let prevHead = this.head;
 
     this.head = this.head.next;
+    if (this.head === null) {
+      this.tail = null;
+    }
 
-    return prevHead;
+    this.length--;
+    return prevHead.val;
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
+    if (idx < 0) throw new Error;
+
+    let currNode = this.head;
+
+    for (let i = 0; i < idx; i++) {
+      if (this.next === null) throw new Error;
+      currNode = currNode.next;
+    }
+
+    return currNode.val;
 
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
+    if (idx < 0) throw new Error;
 
+    let currNode = this.head;
+
+    for (let i = 0; i < idx; i++) {
+      if (this.next === null) throw new Error;
+      currNode = currNode.next;
+    }
+
+    currNode.val = val;
   }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
+    if (idx < 0) throw new Error;
 
+    let node = new Node(val);
+
+    if(idx === 0){
+      node.next = this.head;
+      this.head = node;
+      this.length++;
+      return undefined;
+    }
+
+
+    let currNode = this.head;
+    let prevNode;
+    //console.log("outsideee", currNode)
+    //traverse through list
+    for (let i = 0; i < idx; i++) {
+      if (this.next === null) throw new Error;
+      //console.log(currNode)
+      prevNode = currNode;
+      currNode = currNode.next;
+    }
+
+    //insert
+    prevNode.next = node;
+    node.next = currNode;
+
+    this.length++;
+    return undefined;
   }
 
   /** removeAt(idx): return & remove item at idx, */
@@ -97,7 +176,7 @@ class LinkedList {
   /** average(): return an average of all values in the list */
 
   average() {
-    
+
   }
 }
 
